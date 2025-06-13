aad8d2ee8b742ffd076b2c5b281ecbdfe91d4250
jonathan <johnkord17@gmail.com>
2025-06-12 22:14:58 -0500

    Primer commit - estructura inicial del proyecto

---
+++ src/main/java/com/optimedica/OptimedicaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.optimedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimedicaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimedicaBackendApplication.class, args);
+    }
+
+}
